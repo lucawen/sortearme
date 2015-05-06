f321f818cf9ba48bfe46bf3a41d1d2bb38b0404c
Luca Albuquerque <lucalacerda1@gmail.com>
2015-05-06 18:12:13 -0300

    More edits

--- contato.php
+++ contato.php
@@ -86,7 +86,7 @@
     <main>
         <div class="container">
           <?php
-          if ($sucessMsg){
+          if (isset($sucessMsg)){
             ?>
             <div class="row">
               <div class="col s12 m5 l4 offset-l4">

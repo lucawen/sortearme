921dacedd7b8090dfdad58f3df6c8c842ea56e3a
Luca Albuquerque <lucalacerda1@gmail.com>
2015-05-06 18:37:07 -0300

    br mail

--- contato.php
+++ contato.php
@@ -17,10 +17,10 @@
     $headers  = "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
 
-    $mail_msg = 'Mensagem de contato. Assunto:'.$subject.'\n';
-    $mail_msg .= 'Nome:'.$f_name.' '.$l_name.'\n';
-    $mail_msg .= 'Email:'.$email.'\n';
-    $mail_msg .= 'Mensagem:'.$msg.'\n';
+    $mail_msg = 'Mensagem de contato. Assunto:'.$subject.'<br />';
+    $mail_msg .= 'Nome:'.$f_name.' '.$l_name.'<br />';
+    $mail_msg .= 'Email:'.$email.'<br />';
+    $mail_msg .= 'Mensagem:'.$msg.'<br />';
 
     $mail_provider = 'contato@sortear.me';
     $subject_mail = 'Nova mensagem de contato de '.$f_name.' '.$l_name;
